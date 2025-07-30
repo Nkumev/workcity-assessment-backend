459e9c3ac7da1567e87aaa2972e985369e782fe4
Valentine Nkume <vnkume@gmail.com>
2025-07-30 19:13:45 +0100

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(
   cors({
-    origin: "http://localhost:3991",
+    origin: "*",
     credentials: true,
     methods: "*",
     allowedHeaders: "*",
